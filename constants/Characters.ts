eb688092bf02db4701c9ce699e31d0962fc0c9dc
Mitch <mitchy08@hotmail.co.uk>
2024-03-14 00:38:09 +0000

    Initialize

---
+++ constants/Characters.ts
@@ -0,0 +1,58 @@
+const Characters = [
+  {
+    name: 'Katalina',
+  },
+  {
+    name: 'Rackam',
+  },
+  {
+    name: 'Io',
+  },
+  {
+    name: 'Eugen',
+  },
+  {
+    name: 'Rosetta',
+  },
+  {
+    name: 'Lancelot',
+  },
+  {
+    name: 'Vane',
+  },
+  {
+    name: 'Percival',
+  },
+  {
+    name: 'Siegfried',
+  },
+  {
+    name: 'Charlotta',
+  },
+  {
+    name: 'Yodarha',
+  },
+  {
+    name: 'Narmaya',
+  },
+  {
+    name: 'Zeta',
+  },
+  {
+    name: 'Vasaraga',
+  },
+  {
+    name: 'Ferry',
+  },
+  {
+    name: 'Ghandaghoza',
+  },
+  {
+    name: 'Cagliostro',
+  },
+  {
+    name: 'Id',
+  },
+];
+
+export default Characters;
